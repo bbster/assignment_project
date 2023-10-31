435120bcc542a0502ec71d1defa30be537fdd7c6
kimyongkuk <k0603156@gmail.com>
2023-11-01 03:08:44 +0900

    fix: production api url

--- client/src/composables/useFetchCompanies.ts
+++ client/src/composables/useFetchCompanies.ts
@@ -1,5 +1,5 @@
 const BASE_API_URL =
-  process.env.NODE_ENV === 'production' ? 'localhost' : 'http://49.1.213.232:8000/api/v1'
+  process.env.NODE_ENV === 'production' ? 'http://localhost' : 'http://49.1.213.232:8000/api/v1'
 
 export const useFetchCompanies = () => {
   // const fetcher = createFetch({
