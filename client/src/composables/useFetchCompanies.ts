37c177fcfc9f3a0705a7b1e2152595c3f53fb0a0
kimyongkuk <k0603156@gmail.com>
2023-11-01 02:19:17 +0900

    fix(baseUrl): poduction에서 localhost

--- client/src/composables/useFetchCompanies.ts
+++ client/src/composables/useFetchCompanies.ts
@@ -1,8 +1,10 @@
 import { createFetch } from '@vueuse/core'
 
-const BASE_API_URL = 'http://49.1.213.232:8000/api/v1'
+const BASE_API_URL =
+  process.env.NODE_ENV === 'production' ? 'http://localhost' : 'http://49.1.213.232:8000/api/v1'
 
 export const useFetchCompanies = () => {
+  console.log(process.env.NODE_ENV)
   const fetcher = createFetch({
     baseUrl: BASE_API_URL,
     fetchOptions: {},
