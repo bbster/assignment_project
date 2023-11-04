24c6bb4aee5468d4a9c5f047b1adfd07923f31e2
bbster <bbster12@gmail.com>
2023-11-04 18:13:04 +0900

    ruff

--- server/assignment_project/urls.py
+++ server/assignment_project/urls.py
@@ -3,9 +3,6 @@ from drf_spectacular.views import SpectacularAPIView
 from django.contrib import admin
 from django.urls import path
 
-from django.conf import settings
-from django.conf.urls.static import static
-
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
